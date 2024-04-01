f3b95840e303d6841cab732f9463b7e20743a7c8
sleepynix <leapintotheboundless@mailbox.org>
2024-04-01 10:49:11 +0200

    switch to tlp instead of auto-cpufreq

--- common/system/battery.nix
+++ common/system/battery.nix
@@ -2,12 +2,15 @@
 
 {
   /* ---- POWER MANAGEMENT ---- */
-  # Use 1) auto-cpufreq OR 2) TLP (instead of default Gnome PPD)
+  # Select only one of these: 2) auto-cpufreq OR 3) TLP
+  # instead of default 1) Gnome PPD
+  
+  # 1) GNOME PPD
   services.power-profiles-daemon.enable = false;
   
-  # 1) auto-cpufreq
+  # 2) auto-cpufreq
   services.auto-cpufreq = {
-    enable = true;
+    enable = false;
     settings = {
       battery = {
         governor = "powersave";
@@ -20,25 +23,28 @@
     };
   };
   
-  # 2) TLP
-  /* services.tlp = {
+  # 3) TLP
+  # see https://linrunner.de/tlp/support/optimizing.html
+  # and https://linrunner.de/tlp/settings/processor.html
+  services.tlp = {
     enable = true;
     settings = {
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
 
-      CPU_SCALING_GOVERNOR_ON_AC = "balanced";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance"; # or "performance"
       
       # Optional: helps save long term battery health
       # START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
       # STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
     };
-  }; */
+  };
   
+  /* ---- ADDITIONAL TOOLS ---- */
   # Power monitoring and analysis tool
   powerManagement.powertop.enable = true; # --auto-tune enabled by default
   

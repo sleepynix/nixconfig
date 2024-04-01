c62fbe6bb84ac69c7fcf3d4fd9375de317cf81d8
sleepynix <leapintotheboundless@mailbox.org>
2024-04-01 11:24:25 +0200

    tweaked gpu settings

--- common/system/gpu/intel/default.nix
+++ common/system/gpu/intel/default.nix
@@ -2,9 +2,11 @@
 
 {
   /* ---- GPU ACCELERATION ---- */
+  # see https://nixos.org/manual/nixos/stable/index.html#sec-gpu-accel
   hardware.opengl = {
     enable = true;
     driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
       intel-compute-runtime
       intel-media-driver

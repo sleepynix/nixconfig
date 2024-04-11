f6fdc06283249b244376c52ac55102708c00da72
sleepynix <leapintotheboundless@mailbox.org>
2024-04-11 15:15:49 +0200

    added declarative printer config

--- common/system/printing.nix
+++ common/system/printing.nix
@@ -12,5 +12,16 @@
       nssmdns = true; # enable the mDNS NSS plug-in
       openFirewall = true; # open the firewall for UDP port 5353
     };
+    hardware.printers = {
+      ensurePrinters = [
+        name = "HL-2270DW"
+        description = "Brother HL-2270DW";
+        deviceUri = "ipp://192.168.2.107";
+        model = "drv:///brlaser.drv/br2270dw.ppd";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      ];
+    };
   };
 }

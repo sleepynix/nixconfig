5daf40f1c24cda94d2741753b7aa47562b34ab49
sleepynix <leapintotheboundless@mailbox.org>
2024-04-25 20:31:12 +0200

    added some descriptions

--- common/system/security.nix
+++ common/system/security.nix
@@ -1,7 +1,9 @@
 {
   /* ---- SECURITY ---- */
-  security.auditd.enable = true;
-  services.sysstat.enable = true;
+  security.auditd.enable = true; # enable the Linux Audit daemon
+  services.sysstat.enable = true; # performance monitoring tools
+  
+  # disable unused protocols:
   boot.extraModprobeConfig = ''
     install tipc /bin/true
     install rds /bin/true

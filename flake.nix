8eb620c3eb716d9e72479afc54e902d77265df28
sleepynix <leapintotheboundless@mailbox.org>
2025-07-08 21:19:58 +0200

    replaced nixpkgs-unstable with unfree version from numtide

--- flake.nix
+++ flake.nix
@@ -44,10 +44,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:numtide/nixpkgs-unfree?ref=nixos-unstable"; # alternative to "github:NixOS/nixpkgs/nixos-unstable";
     nixvim = {
       url = "github:nix-community/nixvim/nixos-25.05";
-      # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

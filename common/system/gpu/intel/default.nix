400c61d373b054e018591114e291fadca6946082
sleepynix <leapintotheboundless@mailbox.org>
2025-07-08 21:17:44 +0200

    adjusted renamed options

--- common/system/gpu/intel/default.nix
+++ common/system/gpu/intel/default.nix
@@ -3,10 +3,9 @@
 {
   /* ---- GPU ACCELERATION ---- */
   # see https://nixos.org/manual/nixos/stable/index.html#sec-gpu-accel
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       intel-compute-runtime
       intel-media-driver
